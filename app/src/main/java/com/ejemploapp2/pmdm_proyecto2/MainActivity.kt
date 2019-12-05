1f958894015dd164f3d093d38723620d8c9d3da4
lvaqueiroperez <lvaqueiroperez@danielcastelao.org>
2019-12-05 13:45:59 +0100

    "Inicio"

---
+++ app/src/main/java/com/ejemploapp2/pmdm_proyecto2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ejemploapp2.pmdm_proyecto2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
