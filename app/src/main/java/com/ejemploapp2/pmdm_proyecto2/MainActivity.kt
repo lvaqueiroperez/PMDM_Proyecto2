f64abcd06914ed9267f6fb2f3c7a5c47701e2345
lvaqueiroperez <lvaqueiroperez@danielcastelao.org>
2019-12-05 14:26:19 +0100

    "Modificación Interfaz MainActivity"

--- app/src/main/java/com/ejemploapp2/pmdm_proyecto2/MainActivity.kt
+++ app/src/main/java/com/ejemploapp2/pmdm_proyecto2/MainActivity.kt
@@ -2,11 +2,14 @@ package com.ejemploapp2.pmdm_proyecto2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//USAREMOS UNA VARIABLE PARA SABER SI SE HA COMPLETADO UNA PRUEBA O NO, SI SE HA COMPLETADO CAMBIAREMOS EL COLOR DEL BOTOÓN EN LA MAIN_ACTIVITY
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
