b535767fe6d14d9dc33f6dbbfbc4d3d474eeeed5
lvaqueiroperez <lvaqueiroperez@danielcastelao.org>
2019-12-06 14:29:16 +0100

    "Configuración Reto Internet - Bases"

--- app/src/main/java/com/ejemploapp2/pmdm_proyecto2/MainActivity.kt
+++ app/src/main/java/com/ejemploapp2/pmdm_proyecto2/MainActivity.kt
@@ -23,7 +23,6 @@ class MainActivity : AppCompatActivity() {
 
         toast("BIENVENIDO :D")
 
-
     }
 
     fun accesoRetoInternet() {
--- app/src/main/java/com/ejemploapp2/pmdm_proyecto2/RetoInternet.kt
+++ app/src/main/java/com/ejemploapp2/pmdm_proyecto2/RetoInternet.kt
@@ -2,11 +2,36 @@ package com.ejemploapp2.pmdm_proyecto2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_reto_internet.*
+import org.jetbrains.anko.toast
 
 class RetoInternet : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reto_internet)
+
+        toast("RETO INTERNET")
+
+        btnOk.setOnClickListener{comprobarRespuesta()}
+    }
+
+
+    fun comprobarRespuesta(){
+
+        if(txtInputRespuesta2.text.toString() == "21,19618"){
+
+            toast("HAS ACERTADO !")
+
+
+
+        }else{
+
+            toast("RESPUESTA INCORRECTA")
+
+        }
+
+
+
     }
 }
