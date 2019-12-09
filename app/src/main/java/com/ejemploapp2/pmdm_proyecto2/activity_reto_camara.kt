603e52cbecdcfb7e3f6d0872bead95e8b3d0ac1a
lvaqueiroperez <lvaqueiroperez@danielcastelao.org>
2019-12-09 09:20:10 +0100

    "Creación de la activity: RetoCamara"

---
+++ app/src/main/java/com/ejemploapp2/pmdm_proyecto2/activity_reto_camara.kt
@@ -0,0 +1,12 @@
+package com.ejemploapp2.pmdm_proyecto2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_reto_camara : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reto_camara)
+    }
+}
