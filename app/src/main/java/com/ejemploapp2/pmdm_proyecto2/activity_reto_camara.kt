a56b834194e605f18831e84f4a06d335ad7560ee
lvaqueiroperez <lvaqueiroperez@danielcastelao.org>
2019-12-09 09:53:46 +0100

    "Codificación del RetoCamara"

--- app/src/main/java/com/ejemploapp2/pmdm_proyecto2/activity_reto_camara.kt
+++ app/src/main/java/com/ejemploapp2/pmdm_proyecto2/activity_reto_camara.kt
@@ -1,7 +1,19 @@
 package com.ejemploapp2.pmdm_proyecto2
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_reto_camara.*
+import org.jetbrains.anko.toast
+
+const val MY_PERMISSIONS_REQUEST_CAMERA = 1;
+const val REQUEST_IMAGE_CAPTURE = 2;
 
 class activity_reto_camara : AppCompatActivity() {
 
@@ -9,7 +21,60 @@ class activity_reto_camara : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reto_camara)
 
+        toast("RETO CÁMARA")
+
+        btnCamara.setOnClickListener { sacarFoto() }
+
+    }
+
+    //HACE FALTA ESPECIFICAR EN EL MANIFEST !!!
+    fun sacarFoto() {
+
+        //COMPROBAMOS PERMISOS
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            toast("HACE FALTA DAR PERMISO")
+
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.CAMERA), MY_PERMISSIONS_REQUEST_CAMERA
+            )
+
+
+        } else {
+
+            toast("PERMISO ACTIVO")
+
+            //EJECUTAMOS LA ACTIVITY DE LA CÁMARA UNA VEZ TENGAMOS EL PERMISO
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                takePictureIntent.resolveActivity(packageManager)?.also {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+
+
+        }
 
 
     }
+
+    //PARA RECOGER LA FOTO: (se ha suprimido un error)
+    @SuppressLint("MissingSuperCall")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            //EN KOTLIN, CUANDO MANEJAMOS VARIABLES QUE PUEDEN SER NULL (data en este caso),
+            //KOTLIN SIEMPRE NOS OBLIGARÁ A TRATAR ESOS VALORES QUE PUEDEN SER NULL DE ALGUNA MANERA
+            //(por ej, con un "if" que controle si la variable es null o no (if...else...))
+            //SI DE ALGUNA MANERA SABEMOS QUE ESA VARIABLE NO VA A SER NULL NUNCA, PODEMOS USAR EL OPERADOR "!!"
+
+            val imageBitmap = data!!.extras!!.get("data") as Bitmap
+            imageCamara.setImageBitmap(imageBitmap)
+        }
+    }
+
+
 }
