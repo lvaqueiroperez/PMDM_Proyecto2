35646233d2ce3dba42415c6b1b7443f6378075b7
lvaqueiroperez <lvaqueiroperez@danielcastelao.org>
2019-12-10 20:52:06 +0100

    "Creación de la activity RetoMates"

---
+++ app/src/main/java/com/ejemploapp2/pmdm_proyecto2/RetoMates.kt
@@ -0,0 +1,12 @@
+package com.ejemploapp2.pmdm_proyecto2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RetoMates : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reto_mates)
+    }
+}
